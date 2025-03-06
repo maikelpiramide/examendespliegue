947de7f69181ecf658fba1c54acb88f5577e3400
Maikel <230254@fppiramide.com>
2025-03-06 11:16:58 +0100

    bbdd cmabiada

--- context/MysqlConnector.ts
+++ context/MysqlConnector.ts
@@ -8,10 +8,10 @@ let connection: mysql.Connection;
 export const initializeConnection = async (): Promise<void> => {
   if (!connection) {
     connection = await mysql.createConnection({
-      host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
+      host: "database-1.c1kso4sie8xb.us-east-1.rds.amazonaws.com",
+      user: "admin",
+      password: "Code0clase$$",
+      database: "pics",
     });
     console.log('Conexión a la base de datos MySql establecida');
   }
